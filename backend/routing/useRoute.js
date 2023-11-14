359417e437916f5981e1bb96b50e7eb005bc4cfe
harshit-506 <122781586+harshit-506@users.noreply.github.com>
2023-11-15 01:43:21 +0530

    login-done

--- backend/routing/useRoute.js
+++ backend/routing/useRoute.js
@@ -42,6 +42,22 @@ router.post('/register', async (req, res) => {
         res.status(500).json({ message: 'Registration failed' });
     }
 });
+router.post('/login',async(req,res)=>{
+    const {email,password}=req.body;
+    if(!email && !password){
+        return res.status(400).json({message:"Email and Password are required"});
+    }
+    const user=await User.findOne({email});
+    if(!user){
+        return res.status(404).json({ message: 'User not found' });
+    }
+    const passwordMatch=await bcrypt.compare(password,user.password);
+    if(!passwordMatch){
+        return res.status(401).json({ message: 'Incorrect password' });
+       }
+       const token=jwt.sign({email:user.email},secretKey,{expiresIn: '1h'})
+   res.status(200).json({ message: 'Login successful', token });
+})
 
 
 
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,10 +1,48 @@
 import React, { useState } from 'react';
-import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react';
+import axios from 'axios';
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, Toast, VStack, useToast } from '@chakra-ui/react';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [show, setShow] = useState(false);
+  const toast=useToast()
+  const [show,setShow]=useState(false)
+  const [user,setUser]=useState({
+    email:'',
+    password:'',
+  });
+
+  const handleChange = (e, fieldName) => {
+    const { value } = e.target;
+    setUser((prevUser) => ({ ...prevUser, [fieldName]: value }));
+  };
+const submitHandler=async()=>{
+try {
+  const response=await axios.post('http://localhost:5000/api/user/login',{
+    email:user.email,
+    password:user.password,
+  });
+  if(response.status===200){
+    const data=response.data;
+    toast({
+      title: "Login Successful",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+      position: "bottom",
+    });
+  }
+} catch (error) {
+  console.error(error);
+  toast({
+    title: "Email or Password are incorrect",
+    status: "error",
+    duration: 5000,
+    isClosable: true,
+    position: "bottom",
+  });
+}
+}
+
+
 
   const handleClick = () => {
     setShow(!show);
@@ -15,21 +53,21 @@ const Login = () => {
       <FormControl id='email-input' isRequired>
         <FormLabel>Email</FormLabel>
         <Input
-          type='email'
-          placeholder='Enter Your Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+  type='email'
+  placeholder='Enter Your Email'
+  value={user.email}
+  onChange={(e) => handleChange(e, 'email')}
+/>
       </FormControl>
       <FormControl id='password-login' isRequired>
         <FormLabel>Password</FormLabel>
         <InputGroup>
-          <Input
-            type={show ? 'text' : 'password'}
-            placeholder='Enter Your Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <Input
+  type={show ? 'text' : 'password'}
+  placeholder='Enter Your Password'
+  value={user.password}
+  onChange={(e) => handleChange(e, 'password')}
+/>
           <InputRightElement width='4.5rem'>
             <Button h='1.75rem' size='sm' onClick={handleClick}>
               {show ? 'Hide' : 'Show'}
@@ -37,7 +75,9 @@ const Login = () => {
           </InputRightElement>
         </InputGroup>
       </FormControl>
-      <Button colorScheme='blue' width='100%' style={{ marginTop: 15 }}>
+      <Button colorScheme='blue' width='100%' style={{ marginTop: 15 }}
+      onClick={submitHandler}
+      >
         Login
       </Button>
       <Button variant='solid' colorScheme='red' width='100%'>
